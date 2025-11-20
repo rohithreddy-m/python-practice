fc6ce53c6a7f3aa573ac488852347dd8b9975aee
rohith <mallidisairohithreddy@gmail.com>
2025-11-20 13:45:13 +0530

    LinearSearch is Done

--- 8.python.Algorithms/1.LinearSearch.py
+++ 8.python.Algorithms/1.LinearSearch.py
@@ -1,10 +1,10 @@
-def LS(List,Number):
+def LS(List,number):
     for i in range(len(List)):
-        if List[i]==Number:
+        if List[i]==number:
             return i
     return ("The Number is Not Found in given list")
 
 List=[int(i) for i in input("Give  the Number=").split()]
-Number=int(input("Give the Number="))
-result=LS(List,Number)
-print(f"The list is = {List} and Number to find ={Number}.The index={result}")
+number=int(input("Give the Number="))
+result=LS(List,number)
+print(f"The list is = {List} and Number to find ={number}.The index={result}")
