8ae40f71df6e8cf184fc303cf18687a1e0a358f9
rohith <mallidisairohithreddy@gmail.com>
2025-12-24 21:41:27 +0530

    All is Done

--- 1.python.ifStatement/16.IncomeTaxCalculator.py
+++ 1.python.ifStatement/16.IncomeTaxCalculator.py
@@ -5,9 +5,9 @@ if total_amount_to_pay_tax<=250000:
     print(f"Your income is {total_amount_to_pay_tax} that was less than 2,50,000 you do not need to pay tax")
 else:
     if total_amount_to_pay_tax<=500000:
-    total=(total_amount_to_pay_tax-250000)*(5/100)
+        total=(total_amount_to_pay_tax-250000)*(5/100)
     elif total_amount_to_pay_tax<=1000000:
-    total=(250000)*(5/100)+(total_amount_to_pay_tax-500000)*(20/100)
+        total=(250000)*(5/100)+(total_amount_to_pay_tax-500000)*(20/100)
     else :
-    total=(250000)*(5/100)+(500000)*(20/100)+(total_amount_to_pay_tax-1000000)*(30/100)
-    print(f"Your total income is {total_amount_to_pay_tax}. your tax amount is {total}")
+        total=(250000)*(5/100)+(500000)*(20/100)+(total_amount_to_pay_tax-1000000)*(30/100)
+print(f"Your total income is {total_amount_to_pay_tax}. your tax amount is {total}")
